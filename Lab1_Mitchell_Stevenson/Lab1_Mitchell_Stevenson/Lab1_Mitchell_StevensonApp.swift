6eae600dd3e5883b82292a5f2500134fa3d5661d
Erin Kelly <erinkelly@Erins-MacBook-Air.local>
2026-02-16 14:06:02 -0500

    Initial project structure

---
+++ Lab1_Mitchell_Stevenson/Lab1_Mitchell_Stevenson/Lab1_Mitchell_StevensonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Mitchell_StevensonApp.swift
+//  Lab1_Mitchell_Stevenson
+//
+//  Created by Erin Kelly  on 2026-02-16.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Mitchell_StevensonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
