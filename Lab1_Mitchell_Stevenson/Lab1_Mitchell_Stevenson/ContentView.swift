1df0f73101344129c34b523ae56682d87544ec53
Erin Kelly <erinkelly@Erins-MacBook-Air.local>
2026-02-16 14:10:04 -0500

    Added state variables

--- Lab1_Mitchell_Stevenson/Lab1_Mitchell_Stevenson/ContentView.swift
+++ Lab1_Mitchell_Stevenson/Lab1_Mitchell_Stevenson/ContentView.swift
@@ -6,6 +6,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currNum: Int = Int.random(in: 2...200)
+    
+    @State private var correctCount: Int = 0
+    @State private var wrongCount: Int = 0
+    
+    @State private var guesses: Int = 0
+    
+    @State private var wasPrime: Bool? = nil
+    @State private var showFeedback: Bool = false
+    
+    @State private var seconds: Int = 5
+    @State private var timer: Timer? = nil
+    @State private var hasAnswered: Bool = false
+    
+    @State private var showSummary: Bool = false
+    @State private var lastRoundCorrect: Int = 0
+    @State private var lastRoundWrong: Int = 0
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
